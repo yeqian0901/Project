d56f6150c3cae33c096a9ea1239da564d1fb036e
yhe <irishe2804@gmail.com>
2021-10-26 17:17:23 +0200

    First commit

---
+++ squelette1FRog/src/frog/Frog.java
@@ -0,0 +1,30 @@
+package frog;
+
+import gameCommons.Game;
+import gameCommons.IFrog;
+import util.Case;
+import util.Direction;
+
+public class Frog implements IFrog {
+	
+	private Game game;
+
+	public Frog(Game game) {
+	}
+
+
+	@Override
+	public Case getPosition() {
+		return null;
+	}
+
+	@Override
+	public Direction getDirection() {
+		return null;
+	}
+
+	@Override
+	public void move(Direction key) {
+
+	}
+}
